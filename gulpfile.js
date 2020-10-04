4414d4cbf0db19796f305412ba638a1234398703
htmlpluscss <79198889134@ya.ru>
2020-10-04 14:49:01 +0300

    js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,9 @@
 'use strict';
 
 const gulp             = require('gulp');
-const babel            = require('gulp-babel');
 const postcss          = require('gulp-postcss');
 const autoprefixer     = require("autoprefixer");
 const csso             = require("gulp-csso");
-const minify           = require('gulp-minify');
 const browserReporter  = require('postcss-browser-reporter');
 
 const mqpacker         = require("css-mqpacker");
@@ -26,12 +24,7 @@ const del              = require('del');
 
 const newer            = require('gulp-newer');
 
-const concat           = require('gulp-concat');
-const gulpif           = require('gulp-if');
-const remember         = require('gulp-remember');
-
 const debug            = require('gulp-debug');
-const touch            = require('gulp-touch');
 
 const w3cjs            = require('gulp-w3cjs');
 
@@ -85,7 +78,29 @@ gulp.task('html', function() {
 gulp.task('html-touch', function() {
 
 	return gulp.src('src/**/index.html')
-		.pipe(touch());
+		.pipe(plumber())
+		.pipe(debug({title: 'html:'}))
+		.pipe(nunjucksRender({
+			data: {
+				url: 'http://' + domain,
+				site: site
+			},
+			path: 'src/'
+		}))
+		.pipe(w3cjs({
+			verifyMessage: function(type, message) {
+
+				// prevent logging error message
+				if(message.indexOf('Attribute “loading” not allowed on element “img” at this point.') === 0) return false;
+
+				if(message.indexOf('iframe') !== -1) return false;
+
+				// allow message to pass through
+				return true;
+			}
+		}))
+		.pipe(w3cjs.reporter())
+		.pipe(gulp.dest('build'))
 
 });
 
@@ -104,67 +119,14 @@ gulp.task('css', function () {
 			.pipe(gulp.dest('build/css'))
 			.pipe(postcss([
 				autoprefixer({
-					browsers: 'Android >= 4.4'
+					browsers: 'Android >= 5'
 				})
 			]))
 			.pipe(csso())
 			.pipe(rename({suffix: ".min"}))
-//			.pipe(gulp.dest('src/css'))
 			.pipe(gulp.dest('build/css'))
 
 });
-
-gulp.task('babel', function() {
-
-	return gulp.src(['src/js/js.js','src/js/*.js','!src/js/*.min.js'])
-		.pipe(debug({title: 'babel'}))
-		.pipe(sourcemaps.init())
-		.pipe(concat('_js.js'))
-		.pipe(sourcemaps.write())
-		.pipe(babel({
-			presets: ['@babel/env']
-		}))
-		.pipe(minify({
-			preserveComments: "some",
-			ext : {
-				min:'.min.js'
-			}
-		}))
-		.pipe(gulp.dest('build/js/'))
-
-});
-
-gulp.task('min', function() {
-
-	return gulp.src(['src/js/min/*.js','!src/js/min/swiper.min.js'])
-		.pipe(debug({title: 'min'}))
-		.pipe(gulpif(
-			function(file){
-				return !(/min$/.test(file.stem));
-			},
-			minify({
-				noSource: true
-			})
-		))
-		.pipe(concat('_min.js'))
-		.pipe(gulp.dest('build/js'))
-
-});
-
-gulp.task('concat', function() {
-
-	gulp.src(['build/js/_min.js','build/js/_js.js'])
-		.pipe(concat('scripts.js'))
-		.pipe(gulp.dest('build/js'));
-
-	return gulp.src(['build/js/_min.js','build/js/_js.min.js'])
-		.pipe(concat('scripts.min.js'))
-//		.pipe(gulp.dest('src/js'))
-		.pipe(gulp.dest('build/js'))
-
-});
-
-gulp.task('js', gulp.series('babel','min','concat'));
 
 gulp.task('serve', function() {
 
@@ -172,7 +134,7 @@ gulp.task('serve', function() {
 		server: 'build',
 		files: [
 			{
-				match: ['build/**/*.*', '!build/**/*.min.{css,js}'],
+				match: ['build/**/*.*', '!build/**/*.min.{css}'],
 				fn: function (event, file) {
 					this.reload()
 				}
@@ -189,19 +151,9 @@ gulp.task('clear', function() {
 
 });
 
-gulp.task('copy-js', function() {
-
-// big scripts
-	return gulp.src([
-		'src/js/min/swiper.min.js',
-		])
-		.pipe(gulp.dest('build/js'));
-
-});
-
 gulp.task('copy', function() {
 
-	return gulp.src(['src/**/*.*', '!src/**/*.{css,html,js}'], {since: gulp.lastRun('copy')})
+	return gulp.src(['src/**/*.*', '!src/**/*.{css,html}'], {since: gulp.lastRun('copy')})
 			.pipe(debug({title: 'copy:'}))
 			.pipe(newer('build'))
 			.pipe(debug({title: 'copy:newer'}))
@@ -219,30 +171,26 @@ gulp.task('ftp', function () {
 
 	const f = filter('**/*.html', {restore: true});
 
-//	return gulp.src('build/**/*.{css,html,js}', {since: gulp.lastRun('ftp')})
 	return gulp.src('build/**/*', {since: gulp.lastRun('ftp')})
 		.pipe(debug({title: 'ftp:'}))
 		.pipe(f)
 		.pipe(replace('css/styles.css', 'css/styles.min.css?' + Date.now()))
-		.pipe(replace('js/scripts.js', 'js/scripts.min.js?' + Date.now()))
 		.pipe(f.restore)
 		.pipe(ftp(config.ftp));
 
 });
 
 gulp.task('watch', function() {
-	gulp.watch(['src/js/*.*','!src/js/scripts.min.js'], gulp.series('js'));
 	gulp.watch(['src/css/*.*','!src/css/styles.min.css'], gulp.series('css'));
 	gulp.watch('src/**/index.html', gulp.series('html'));
 	gulp.watch(['src/**/*.html','!src/**/index.html'], gulp.series('html-touch'));
-	gulp.watch(['src/**/*.*', '!src/**/*.{css,html,js}'], gulp.series('copy'));
+	gulp.watch(['src/**/*.*', '!src/**/*.{css,html}'], gulp.series('copy'));
 	gulp.watch('build/**/*.*', gulp.series('ftp'));
 });
 
 gulp.task('default', gulp.series(
 	'clear',
-	'copy-js',
-	gulp.parallel('css','js'),
+	'css',
 	'html',
 	'copy',
 	gulp.parallel('ftp','watch','serve')
--- src/js/menu.js
+++
@@ -1,7 +0,0 @@
-((btn) => {
-
-	"use strict";
-
-	btn.addEventListener('click', () => document.body.classList.toggle('menu-show'));
-
-})(document.querySelector('.btn-menu-toggle'));
